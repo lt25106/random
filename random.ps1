c7a04b9ff6419e40655ad803015c27e040f35183
name <11pro12ispro@gmail.com>
2025-08-17 16:05:32 +0800

    Create random.ps1

---
+++ random.ps1
@@ -0,0 +1,15 @@
+$answer = Get-Random -Minimum 0 -Maximum 100
+$count = 1
+while ($true) {
+  $guess = Read-Host "Guess a number between 0 and 100"
+  if ($guess -gt $answer) {
+    Write-Host "Too high! Try again."
+  } elseif ($guess -lt $answer) {
+    Write-Host "Too low! Try again."
+  } else {
+    Write-Host "Congratulations! You guessed the number."
+    Write-Host "It took you $count tries."
+    break
+  }
+  $count++
+}
