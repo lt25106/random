23a37937ea2d878b0f46d654afcca980050fe675
Spleefies <cutecatty32@gmail.com>
2025-08-17 11:24:12 +0800

    java.java and kotlin.kt
    Too High

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = (int) (Math.random() * 102);
+        int attempts = 0;
+
+        Scanner myObj = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("guess a number from 0 to 102:");
+            int guess = myObj.nextInt();
+            attempts++;
+            if (guess < a) {
+                System.out.println("too low");
+            } else if (guess > a) {
+                System.out.println("Too High");
+            } else {
+                System.out.println("You took " + attempts + " attempts");
+                break;
+            }
+        }
+    }
+}
