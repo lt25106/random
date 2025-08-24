73c05747a446a2718ec001b3c9bcbf0b87a3ffe7
name <11pro12ispro@gmail.com>
2025-08-24 17:34:58 +0800

    Create random.cs

---
+++ random.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int ans = random.Next(0, 101);
+int count = 1;
+
+while (true)
+{
+  Console.Write("Guess a number between 0 and 100: ");
+  int guess = int.Parse(Console.ReadLine());
+  if (guess < ans)
+  {
+    Console.WriteLine("Too low!");
+  }
+  else if (guess > ans)
+  {
+    Console.WriteLine("Too high!");
+  }
+  else
+  {
+    Console.WriteLine("Correct!");
+    Console.WriteLine($"Guesses taken: {count}");
+    break;
+  }
+  count++;
+} 
