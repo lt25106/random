29377d766490a40b7e670156798bbaa854f3f920
lt25106 <11pro12ispro@gmail.com>
2026-01-31 16:39:12 +0800

    Standardised Shell version to fit with python version

--- Python.py
+++ Python.py
@@ -4,9 +4,9 @@ count = 1
 while True:
   guess = int(input("Guess the number between 0 and 100: "))
   if guess < answer:
-    print("Too low")
+    print("Too low!")
   elif guess > answer:
-    print("Too High")
+    print("Too High!")
   else:
     print("Correct!")
     print("Guesses taken:", count)
