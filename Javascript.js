90a2878beac7e4b32325caf4c612d80a24260118
lt25106 <11pro12ispro@gmail.com>
2026-01-31 17:03:08 +0800

    Standardised Javascript version to fit with Java version

--- Javascript.js
+++ Javascript.js
@@ -1,28 +1,27 @@
-const readline = require("readline");
+import { createInterface } from 'readline'
 
-const rl = readline.createInterface({
+const rl = createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-const answer = Math.floor(Math.random() * 101);
+const answer = Math.floor(Math.random() * 101)
 let counter = 1
 
 function askGuess() {
-  rl.question("Guess the number between 0 and 100: ", guess => {
+  rl.question("Guess a number between 0 and 100: ", guess => {
     if (guess < answer) {
-      console.log("Too low");
-      counter++;
-      askGuess();
+      console.log("Too low!")
+      counter++
+      askGuess()
     } else if (guess > answer) {
-      console.log("Too high");
-      counter++;
-      askGuess();
+      console.log("Too high!")
+      counter++
+      askGuess()
     } else {
-      console.log("Correct!");
-      console.log("Guesses taken: " + counter);
+      console.log(`Correct! You took ${counter} guesses.`)
     }
-  });
+  })
 }
 
-askGuess();
+askGuess()
